b9b252ba545bdaaa690b85351c86cba3161a71e3
Guilherme Felipe Pereira de Melo <62664524+GuilhermeMelo-sys@users.noreply.github.com>
2020-10-20 20:52:22 -0200

    Criação do metodo para calcular valor de uma lista de pizzas em "Pedido Pizza"

--- src/pizzaria/Model/PedidoPizza.java
+++ src/pizzaria/Model/PedidoPizza.java
@@ -7,6 +7,7 @@ package pizzaria.Model;
 
 import Enum.ConverterPizzasEnum;
 import java.math.BigDecimal;
+import java.util.List;
 import readOnly.PizzaReadOnly;
 import readOnly.PizzaReadOnlyPedido;
 
@@ -20,11 +21,11 @@ public class PedidoPizza {
     private int idPedido;
     private Cobertura cobertura;
     private int id;
-    
-    public void setCobertura(Cobertura cobertura){
+
+    public void setCobertura(Cobertura cobertura) {
         this.cobertura = cobertura;
     }
-    
+
     public Cobertura getCobertura() {
         return cobertura;
     }
@@ -48,25 +49,33 @@ public class PedidoPizza {
     public int getId() {
         return id;
     }
-    
-    public BigDecimal calcularValor(){
-       return getCobertura().getValor().add(getPizza().getValor()
-               .multiply(BigDecimal.valueOf(getPizza().getQuantidade())));
+
+    public BigDecimal calcularValores(List<PedidoPizza> pizzas) {
+        BigDecimal valorDaLista = BigDecimal.ZERO;
+        pizzas.forEach((c) -> {
+            valorDaLista.add(c.calcularValor());
+        });
+        return valorDaLista;
     }
-    
-    public BigDecimal calcularEConverterValor(String tipo){
+
+    public BigDecimal calcularValor() {
+        return getCobertura().getValor().add(getPizza().getValor()
+                .multiply(BigDecimal.valueOf(getPizza().getQuantidade())));
+    }
+
+    public BigDecimal calcularEConverterValor(String tipo) {
         BigDecimal valorConvertido = converterValor(tipo);
-        
+
         return getCobertura().getValor().add(
                 valorConvertido.multiply(BigDecimal.valueOf(getPizza().getQuantidade())));
     }
-    
-    public BigDecimal converterValor(String tipo){
+
+    public BigDecimal converterValor(String tipo) {
         Pizza pizza = Pizza.converter(this);
         return ConverterPizzasEnum.valueOf(getPizza().getTipoPizza().name()).converter(pizza, tipo);
     }
-    
-    public PedidoPizza(PizzaReadOnly pizza, int pedido,  Cobertura cobertura, int quantidade, int id) {
+
+    public PedidoPizza(PizzaReadOnly pizza, int pedido, Cobertura cobertura, int quantidade, int id) {
         this.pizzasPedido = new PizzaReadOnlyPedido(pizza.getId(), pizza.getTipoPizza(),
                 pizza.getSabor(), pizza.getValor(), quantidade);
         this.id = id;
