6d8716ae32e051c15cfa4d662fe3673e9ee2362e
Guilherme Felipe Pereira de Melo <62664524+GuilhermeMelo-sys@users.noreply.github.com>
2020-10-20 19:41:27 -0200

    Criação de uma "UI" para uma scrollBar do lado Horizontal

--- src/pizzaria/View/Util/ConfigurarJScroll.java
+++ src/pizzaria/View/Util/ConfigurarJScroll.java
@@ -18,17 +18,17 @@ import javax.swing.plaf.basic.BasicScrollBarUI;
  * @author Guilh
  */
 public class ConfigurarJScroll {
-       
+
     public static void configJscrollPane(JScrollPane jsb) {
         jsb.getVerticalScrollBar().setBackground(Color.white);
         jsb.getHorizontalScrollBar().setBackground(Color.white);
-        
+
         jsb.getVerticalScrollBar().setUI(new BasicScrollBarUI() {
             @Override
             protected void configureScrollBarColors() {
                 this.thumbDarkShadowColor = Color.gray.brighter();
             }
-            
+
             @Override
             protected JButton createDecreaseButton(int orientation) {
                 return createZeroButton();
@@ -38,7 +38,7 @@ public class ConfigurarJScroll {
             protected JButton createIncreaseButton(int orientation) {
                 return createZeroButton();
             }
-            
+
             private JButton createZeroButton() {
                 JButton jbutton = new JButton();
                 jbutton.setBackground(Color.white);
@@ -46,7 +46,33 @@ public class ConfigurarJScroll {
                 jbutton.setBorder(BorderFactory.createLineBorder(Color.gray.brighter(), 1));
                 return jbutton;
             }
-            
+
+        });
+
+        jsb.getHorizontalScrollBar().setUI(new BasicScrollBarUI() {
+            @Override
+            protected void configureScrollBarColors() {
+                this.thumbDarkShadowColor = Color.gray.brighter();
+            }
+
+            @Override
+            protected JButton createDecreaseButton(int orientation) {
+                return createZeroButton();
+            }
+
+            @Override
+            protected JButton createIncreaseButton(int orientation) {
+                return createZeroButton();
+            }
+
+            private JButton createZeroButton() {
+                JButton jbutton = new JButton();
+                jbutton.setBackground(Color.white);
+                jbutton.setPreferredSize(new Dimension(20, 20));
+                jbutton.setBorder(BorderFactory.createLineBorder(Color.gray.brighter(), 1));
+                return jbutton;
+            }
+
         });
     }
 }
